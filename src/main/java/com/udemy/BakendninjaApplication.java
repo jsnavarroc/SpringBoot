ac1a82d15271cde02a02a4d0027473d018be2ba5
Johan Navarro <21sebas12@gmail.com>
2019-01-22 23:15:06 -0500

    first commit

---
+++ src/main/java/com/udemy/BakendninjaApplication.java
@@ -0,0 +1,14 @@
+package com.udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakendninjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakendninjaApplication.class, args);
+	}
+
+}
+
